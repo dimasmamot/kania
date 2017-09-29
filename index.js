6cd6f953da970d6cd84764d223d93fb73080fe61
Mamot <dimas.rizky.hp@gmail.com>
2017-09-29 08:22:13 +0700

    Add handling error validation

--- index.js
+++ index.js
@@ -3,42 +3,41 @@
 const line = require('@line/bot-sdk');
 const express = require('express');
 
-// create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
 };
 
-// create LINE SDK client
 const client = new line.Client(config);
-
-// create Express app
-// about Express itself: https://expressjs.com/
 const app = express();
 
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
 app.post('/webhook', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
     .then((result) => res.json(result));
 });
 
-// event handler
+app.use((err,req,res,next)=>{
+  if(err instanceof SignatureValidationFailed){
+    res.status(401).send(err.signature);
+    return;
+  }else if(err instanceof JSONParseError){
+    res.status(400).send(err.raw);
+    return;
+  }
+  next(err);
+});
+
 function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
     return Promise.resolve(null);
   }
 
-  // create a echoing text message
   const echo = { type: 'text', text: event.message.text };
 
-  // use reply API
   return client.replyMessage(event.replyToken, echo);
 }
 
-// listen on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
