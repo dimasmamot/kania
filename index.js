34261d4ff167793e108b64a3563986c7d3bca36a
Mamot <dimas.rizky.hp@gmail.com>
2017-09-29 09:00:59 +0700

    Restrukturasi conditional

--- index.js
+++ index.js
@@ -36,15 +36,14 @@ function handleEvent(event) {
   }
 
   const message = event.message;
-  const msg = 'default reply';
 
   if(message.type == 'text' && message.text === 'test'){
     if(event.source.type === 'room'){
-      msg = {type: 'tex', text: "Ini dari room revisi?"};
+      const msg = {type: 'tex', text: "Ini dari room revisi?"};
     }else if(event.source.type === 'group'){
-      msg = {type: 'tex', text: "Ini dari grup?"};
+      const msg = {type: 'tex', text: "Ini dari grup?"};
     }else{
-      msg = {type: 'text',text: message.text};
+      const msg = {type: 'text',text: message.text};
     } 
 
     return client.replyMessage(event.replyToken, msg)
