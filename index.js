d84115b5c1f3e3f0cac3325b1a26a3a63b3b85ae
Mamot <dimas.rizky.hp@gmail.com>
2017-10-03 20:30:43 +0700

    Mysql fix 2

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var con = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env DB_NAME
+  database: process.env.DB_NAME
 });
 
 const config = {
