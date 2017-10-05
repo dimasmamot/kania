7a6470a187dff1f008c0f5d155e0461d22885bcc
Mamot <dimas.rizky.hp@gmail.com>
2017-10-05 12:48:30 +0700

    Request place photo

--- index.js
+++ index.js
@@ -84,6 +84,7 @@ function handleEvent(event) {
     }
 
     if(message.text == 'photo'){
+      console.log("event njaluk foto fired");
       var photoQuery = {
         maxwidth: 400,
         photoreference: "CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU"
@@ -96,7 +97,7 @@ function handleEvent(event) {
         if(err)
           console.log("Error query place photo : ", err);
 
-        console.log(response.json.result);
+        console.log(response);
       });
     }  
 
